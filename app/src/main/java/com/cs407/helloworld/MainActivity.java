2f7afcf6473d8c4bc2d870e7a6a1ff3b1dd6087e
idoll <idollevoet@wisc.edu>
2023-09-14 12:43:41 -0500

    Second version

---
+++ app/src/main/java/com/cs407/helloworld/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cs407.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imgview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
